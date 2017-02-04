7960da420d3a9f647e5cb1f547c0516917860e03
eddy0411 <eddy920411@gmail.com>
2017-02-04 20:32:37 +0800

    建塔和敵人移動

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var FPS = 60
 // 創造 img HTML 元素，並放入變數中
 var bgImg = document.createElement("img");
 var enemyImg = document.createElement("img");
@@ -10,7 +11,6 @@ enemyImg.src = "images/slime.gif";
 btnImg.src = "images/tower-btn.png"
 towerImg .src= "images/tower.png"
 
-
 // 找出網頁中的 canvas 元素
 var canvas = document.getElementById("game-canvas");
 
@@ -18,18 +18,29 @@ var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
 function draw(){
+	enemy.move()
 	// 將背景圖片畫在 canvas 上的 (0,0) 位置
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(enemyImg,enemy.x,enemy.y);
   ctx.drawImage(btnImg,640-64,480-64,64,64)
-  ctx.drawImage(towerImg,cursor.x-15,cursor.y-15)
+  if(isBuilding == true){
+		ctx.drawImage(towerImg,cursor.x-cursor.x%32,cursor.y-cursor.y%32);
+	} else{
+		ctx.drawImage(towerImg,tower.x,tower.y);
+	}
 }
 // 執行 draw 函式
-setInterval(draw, 16);
+setInterval(draw, 1000/FPS);
 
 var enemy = {
 	x: 96,
 	y: 480-32,
+	speedX:0,
+	speedY:-64,
+	move:function(){
+		this.x+=this.speedX/FPS;
+		this.y+=this.speedY/FPS;
+	}
 }
 
 var cursor = {
@@ -37,8 +48,30 @@ var cursor = {
 	y: 0,
 }
 
+var tower = {
+	x: 633,
+	y: 455,
+}
 $("#game-canvas").on("mousemove",mousemove)
 function mousemove(event){
 	cursor.x= event.offsetX
 	cursor.y= event.offsetY
 }
+
+var isBuilding = false;
+
+$("#game-canvas").on("click", mouseclick);
+function mouseclick(){
+	if(cursor.x > 576 && cursor.y > 416){
+		isBuilding = true;
+	} else{
+
+		if (isBuilding = true) {
+			tower.x = event.offsetx - cursor.x%32;
+			tower.y = event.offsety - cursor.y%32;;
+		}
+		// 建造完成
+		isBuilding = false;
+	}
+}
+
