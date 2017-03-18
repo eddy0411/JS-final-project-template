ff430c97515d543bf4d3ad7d971ae37701451ae9
eddy0411 <eddy920411@gmail.com>
2017-03-18 21:29:21 +0800

    塔射擊和蓋多座塔

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 var FPS = 60;
+var scores = 0;
+var money = 60;
 var clock = 0;
+var hp = 100;
 // 創造 img HTML 元素，並放入變數中
 var bgImg = document.createElement("img");
 var enemyImg = document.createElement("img");
 var btnImg = document.createElement("img"); 
-var towerImg = document.createElement("img"); 
+var towerImg = document.createElement("img");
+var crosshairImg = document.createElement("img");
 
 // 設定這個元素的要顯示的圖片
 bgImg.src = "images/map.png";
 enemyImg.src = "images/slime.gif";
 btnImg.src = "images/tower-btn.png";
-towerImg .src= "images/tower.png";
+towerImg.src = "images/tower.png";
+crosshairImg.src = "images/crosshair.png"
 
 // 找出網頁中的 canvas 元素
 var canvas = document.getElementById("game-canvas");
@@ -28,14 +33,32 @@ function draw(){
   ctx.drawImage(bgImg,0,0);
   for (var i = 0; i <enemies.length; i++) {
   	enemies[i].move();
+  	if (enemies[i].hp <= 0) {
+  		enemies.splice(i,1)
+  		scores = scores+10
+  		money = money+30
+  	}
   	ctx.drawImage(enemyImg,enemies[i].x,enemies[i].y);
   }
-  ctx.drawImage(btnImg,640-64,480-64,64,64)
-  if(isBuilding == true){
-		ctx.drawImage(towerImg,cursor.x-cursor.x%32,cursor.y-cursor.y%32);
-	} else{
-		ctx.drawImage(towerImg,tower.x,tower.y);
+if(isBuilding == true){
+	ctx.drawImage(towerImg,cursor.x-cursor.x%32,cursor.y-cursor.y%32);
+}
+	ctx.drawImage(btnImg,640-64,480-64,64,64)	
+for (var i = 0; i < towers.length; i++) {
+
+	ctx.drawImage(towerImg,towers[i].x,towers[i].y);	  
+	towers[i].searchEnemy();
+	if (towers[i].aimingEnemyId != null) {
+	var id = towers[i].aimingEnemyId;
+	ctx.drawImage(crosshairImg,enemies[id].x,enemies[id].y);
 	}
+  }
+  
+	ctx.font="18px Arial";
+	ctx.fillStyie = "white";
+	ctx.fillText("生命樹血量 = " + hp,32,50)
+	ctx.fillText("分數 = " + scores,32+150,50)
+	ctx.fillText("錢 = " + money,32+270,50)
 }
 // 執行 draw 函式
 setInterval(draw, 1000/FPS);
@@ -53,14 +76,25 @@ var enemyPath = [
 function Enemy(){
 	this.x = 96;
 	this.y = 480-32;
+	this.hp = 10
 	this.speedX = 0;
 	this.speedY = -64;
 	this.pathDes = 0;
-	this.move = function(){
+	 this.move = function(){
 		if(isCollided(enemyPath[this.pathDes].x,enemyPath[this.pathDes].y,this.x,this.y,64/FPS,64/FPS)){
+			//移動
 			this.x = enemyPath[this.pathDes].x
 			this.y = enemyPath[this.pathDes].y
 			this.pathDes++
+			if (this.pathDes == enemyPath.length) {
+				this.hp = 0
+				hp -= 10
+				scores = scores-10
+				money = money-30
+				return;
+
+			}
+			//計算和修改
 			if (enemyPath[this.pathDes].y < this.y) {
 				//往上
 				this.speedX=0;
@@ -90,15 +124,49 @@ var cursor = {
 	y: 0,
 }
 
-var tower = {
-	x: 633,
-	y: 455,
+function Tower(){
+	this.x = 0;
+	this.y = -100;
+	this.range =100;
+	this.aimingEnemyId =null,
+	this.searchEnemy = function(){
+		this.readyToshootime -= 1/FPS;
+		for(var i=0; i<enemies.length; i++){
+			var distance = Math.sqrt(Math.pow(this.x-enemies[i].x,2) + Math.pow(this.y-enemies[i].y,2));
+			if (distance<=this.range) {
+				this.aimingEnemyId = i;
+				console.log(this.readyToshootime)
+				if (this.readyToshootime <= 0) {
+					this.shoot(i);
+					this.readyToshootime = this.fireRate;
+				}
+				return;
+			}
+		}
+		// 如果都沒找到，會進到這行，清除鎖定的目標
+		this.aimingEnemyId = null;
+	};
+	this.shoot = function(id){
+		ctx.beginPath();
+		ctx.moveTo(this.x +16, this.y +16);
+		ctx.lineTo(enemies[id].x +16, enemies[id].y +16);
+		ctx.strokeStyle = "yellow";
+		ctx.lineWidth = 3;
+		ctx.stroke()
+		enemies[id].hp -= this.damage;
+	};
+	this.fireRate=1;
+	this.readyToshootime=1;
+	this.damage=5;
 }
+var towers = []
+
 $("#game-canvas").on("mousemove",mousemove)
 function mousemove(event){
 	cursor.x= event.offsetX
 	cursor.y= event.offsetY
 }
+
 
 var isBuilding = false;
 
@@ -107,10 +175,13 @@ function mouseclick(){
 	if(cursor.x > 576 && cursor.y > 416){
 		isBuilding = true;
 	} else{
-
-		if (isBuilding = true) {
-			tower.x = cursor.x - cursor.x%32;
-			tower.y = cursor.y - cursor.y%32;;
+		//
+		if (isBuilding == true) {
+			var newTower = new Tower();
+			newTower.x = cursor.x - cursor.x%32;
+			newTower.y = cursor.y - cursor.y%32;
+			towers.push(newTower)
+			money -=10;
 		}
 		// 建造完成
 		isBuilding = false;
